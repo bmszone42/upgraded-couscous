c9a868d92004d0bf7a36ea33b8125f711c1f87ec
bmszone42 <122175396+bmszone42@users.noreply.github.com>
2023-04-14 17:00:37 -0400

    Added code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,37 @@
 import streamlit as st
 import requests
+from bs4 import BeautifulSoup
 import datetime
-import json
 
 def get_powerball_data(start_date, end_date):
-    url = f"https://api.powerball.net/v1/results?start={start_date}&end={end_date}"
+    url = f"https://www.powerball.com/api/v1/numbers/powerball?_format=json&min=1&max=5&startDate={start_date}&endDate={end_date}"
     response = requests.get(url)
-    return response.json()
+    soup = BeautifulSoup(response.text, "html.parser")
+    data = soup.find_all("div", class_="winning-numbers")
+
+    results = []
+    for draw in data:
+        numbers = [int(n.text) for n in draw.find_all("span", class_="ball")]
+        powerball = int(draw.find("span", class_="powerball").text)
+        results.append({"winning_numbers": set(numbers), "bonus_number": powerball})
+    return results
 
 def get_mega_millions_data(start_date, end_date):
-    url = f"https://api.megamillions.net/v1/results?start={start_date}&end={end_date}"
+    url = f"https://www.megamillions.com/api/v1/numbers/megamillions?_format=json&min=1&max=5&startDate={start_date}&endDate={end_date}"
     response = requests.get(url)
-    return response.json()
+    soup = BeautifulSoup(response.text, "html.parser")
+    data = soup.find_all("div", class_="winning-numbers")
+
+    results = []
+    for draw in data:
+        numbers = [int(n.text) for n in draw.find_all("span", class_="ball")]
+        mega_ball = int(draw.find("span", class_="mega-ball").text)
+        results.append({"winning_numbers": set(numbers), "bonus_number": mega_ball})
+    return results
 
 def check_winning_numbers(lottery_data, user_numbers, bonus_number):
     for draw in lottery_data:
-        winning_numbers = set(draw["winning_numbers"].split(","))
+        winning_numbers = draw["winning_numbers"]
         draw_bonus_number = draw["bonus_number"]
         
         if winning_numbers == user_numbers and draw_bonus_number == bonus_number:
@@ -26,18 +42,33 @@ st.title("Lottery Number Checker")
 st.subheader("Check your Powerball and Mega Millions numbers")
 
 lottery_type = st.selectbox("Select Lottery Type", ["Powerball", "Mega Millions"])
+
 st.write("Enter your numbers (comma separated):")
-user_numbers = set(map(int, st.text_input("").split(",")))
-bonus_number = st.number_input("Enter your bonus number:", min_value=1, max_value=50)
+default_powerball_numbers = "1, 2, 3, 4, 5"
+default_mega_millions_numbers = "6, 7, 8, 9, 10"
+
+if lottery_type == "Powerball":
+    user_numbers_input = st.text_input("", value=default_powerball_numbers)
+else:
+    user_numbers_input = st.text_input("", value=default_mega_millions_numbers)
+user_numbers = set(map(int, user_numbers_input.split(",")))
+
+if lottery_type == "Powerball":
+    bonus_number = st.number_input("Enter your Powerball bonus number:", value=6, min_value=1, max_value=50)
+else:
+    bonus_number = st.number_input("Enter your Mega Millions bonus number:", value=11, min_value=1, max_value=50)
 
 start_date = st.date_input("Select start date:")
 end_date = st.date_input("Select end date:")
 
 if st.button("Check Numbers"):
+    start_date_str = start_date.strftime("%Y-%m-%d")
+    end_date_str = end_date.strftime("%Y-%m-%d")
+    
     if lottery_type == "Powerball":
-        lottery_data = get_powerball_data(start_date, end_date)
+        lottery_data = get_powerball_data(start_date_str, end_date_str)
     else:
-        lottery_data = get_mega_millions_data(start_date, end_date)
+        lottery_data = get_mega_millions_data(start_date_str, end_date_str)
 
     won = check_winning_numbers(lottery_data, user_numbers, bonus_number)
 
