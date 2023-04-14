be9115993eef7eb47adcce0e0b10b99389a9dcdd
bmszone42 <122175396+bmszone42@users.noreply.github.com>
2023-04-14 18:06:24 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,30 +12,30 @@ import datetime
 
 # Scrape Powerball data from official website for a given date range
 def get_powerball_data(start_date, end_date):
-    url = "https://www.powerball.com/draw-games/powerball"
+    url = "https://www.powerball.com/previous-results"
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
-    data = soup.select(".views-row")
+    data = soup.select(".result-item")
     results = []
 
     while True:
         for draw in data:
-            draw_date = draw.select_one(".views-field-field-pb-date").text.strip()
-            draw_date = datetime.datetime.strptime(draw_date, "%B %d, %Y").strftime("%Y-%m-%d")
+            draw_date = draw.select_one(".result-heading").text.strip()
+            draw_date = datetime.datetime.strptime(draw_date, "%m/%d/%Y").strftime("%Y-%m-%d")
 
             if start_date <= draw_date <= end_date:
-                numbers = [int(num.text) for num in draw.select(".field-item.even")[1:-1]]
-                powerball = int(draw.select(".field-item.odd")[-1].text)
+                numbers = [int(num.text) for num in draw.select(".result-ball")]
+                powerball = int(draw.select_one(".result-powerball").text)
 
                 results.append({"date": draw_date, "winning_numbers": set(numbers), "bonus_number": powerball})
 
-        next_page = soup.select_one(".pager-next > a")
+        next_page = soup.select_one(".load-more > a")
         if not next_page:
             break
 
         response = requests.get(next_page["href"])
         soup = BeautifulSoup(response.text, "html.parser")
-        data = soup.select(".views-row")
+        data = soup.select(".result-item")
 
     return results
 
@@ -45,31 +45,31 @@ def get_mega_millions_data(start_date, end_date):
     url = "https://www.megamillions.com/Winning-Numbers/Previous-Drawings.aspx"
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
-    data = soup.select(".page-draw-result")
+    data = soup.select(".row.pb-4.pt-4.border-bottom.border-secondary")
     results = []
 
-    while True:
+     while True:
         for draw in data:
-            draw_date = draw.select_one(".date").text.strip()
-            draw_date = datetime.datetime.strptime(draw_date, "%B %d, %Y").strftime("%Y-%m-%d")
+            draw_date = draw.select_one(".col-sm-6.col-lg-4.pb-2.pb-md-0").text.strip()
+            draw_date = datetime.datetime.strptime(draw_date, "%m/%d/%Y").strftime("%Y-%m-%d")
 
             if start_date <= draw_date <= end_date:
-                numbers_list = draw.select_one(".winning-numbers")
-                numbers = [int(num.text) for num in numbers_list.select(".number-ball")]
-                mega_ball = int(draw.select_one(".megaball").text)
+                numbers_list = draw.select_one(".list-unstyled.winning_numbers")
+                numbers = [int(num.text) for num in numbers_list.select(".ball")] + \
+                          [int(num.text) for num in numbers_list.select(".ball.gold")]
+                mega_ball = int(draw.select_one(".ball.megaball").text)
 
                 results.append({"date": draw_date, "winning_numbers": set(numbers), "bonus_number": mega_ball})
 
-        next_page = soup.select_one(".next > a")
+        next_page = soup.select_one(".pagination > .next > a")
         if not next_page:
             break
 
         response = requests.get(next_page["href"])
         soup = BeautifulSoup(response.text, "html.parser")
-        data = soup.select(".page-draw-result")
+        data = soup.select(".row.pb-4.pt-4.border-bottom.border-secondary")
 
     return results
-
 
 def get_winning_combination(lottery_data, user_numbers, bonus_number):
     for draw in lottery_data:
@@ -100,51 +100,57 @@ def get_winning_combination(lottery_data, user_numbers, bonus_number):
             return "Bonus Only"
     return "No Win"
 
-st.title("Lottery Number Checker")
-st.subheader("Check your Powerball and Mega Millions numbers")
 
-lottery_type = st.selectbox("Select Lottery Type", ["Powerball", "Mega Millions"])
+def app():
+    st.set_page_config(page_title="Lottery Number Checker", page_icon=":money_with_wings:")
 
-st.write("Enter your numbers (comma separated):")
-default_powerball_numbers = "18, 29, 35, 44, 60"
-default_mega_millions_numbers = "8, 33, 38, 50, 53"
+    st.title("Lottery Number Checker")
+    st.subheader("Check your Powerball and Mega Millions numbers")
 
-if lottery_type == "Powerball":
-    user_numbers_input = st.text_input("", value=default_powerball_numbers)
-else:
-    user_numbers_input = st.text_input("", value=default_mega_millions_numbers)
-user_numbers = set(map(int, user_numbers_input.split(",")))
+    lottery_type = st.selectbox("Select Lottery Type", ["Powerball", "Mega Millions"])
 
-if lottery_type == "Powerball":
-    bonus_number = st.number_input("Enter your Powerball bonus number:", value=26, min_value=1, max_value=50)
-else:
-    bonus_number = st.number_input("Enter your Mega Millions bonus number:", value=25, min_value=1, max_value=50)
+    st.write("Enter your numbers (comma separated):")
+    default_powerball_numbers = "18, 29, 35, 44, 60"
+    default_mega_millions_numbers = "8, 33, 38, 50, 53"
 
-start_date = st.date_input("Select start date:")
-end_date = st.date_input("Select end date:")
-
-if st.button("Check Numbers"):
-    start_date_str = start_date.strftime("%Y-%m-%d")
-    end_date_str = end_date.strftime("%Y-%m-%d")
-    
     if lottery_type == "Powerball":
-        lottery_data = get_powerball_data(start_date_str, end_date_str)
+        user_numbers_input = st.text_input("", value=default_powerball_numbers, label_visibility='collapsed')
     else:
-        lottery_data = get_mega_millions_data(start_date_str, end_date_str)
+        user_numbers_input = st.text_input("", value=default_mega_millions_numbers, label_visibility='collapsed')
+    user_numbers = set(map(int, user_numbers_input.split(",")))
 
-    winning_combination = get_winning_combination(lottery_data, user_numbers, bonus_number)
-
-    if winning_combination != "No Win":
-        st.success(f"Congratulations! You got a {winning_combination}!")
+    if lottery_type == "Powerball":
+        bonus_number = st.number_input("Enter your Powerball bonus number:", value=26, min_value=1, max_value=50)
     else:
-        st.warning("Sorry, you did not win.")
-        
-    st.subheader("Numbers drawn for the queried date range:")
-        
-    data = {
-        "Date": [draw["date"] for draw in lottery_data],
-        "Numbers Drawn": [", ".join(str(num) for num in sorted(draw["winning_numbers"])) for draw in lottery_data],
-        "Bonus Number": [draw["bonus_number"] for draw in lottery_data],
-    }
-    df = pd.DataFrame(data)
-    st.write(df)
+        bonus_number = st.number_input("Enter your Mega Millions bonus number:", value=25, min_value=1, max_value=50)
+
+    start_date = st.date_input("Select start date:")
+    end_date = st.date_input("Select end date:")
+
+    if st.button("Check Numbers"):
+        start_date_str = start_date.strftime("%Y-%m-%d")
+        end_date_str = end_date.strftime("%Y-%m-%d")
+
+        if lottery_type == "Powerball":
+            lottery_data = get_powerball_data(start_date_str, end_date_str)
+        else:
+            lottery_data = get_mega_millions_data(start_date_str, end_date_str)
+
+        winning_combination = get_winning_combination(lottery_data, user_numbers, bonus_number)
+
+        if winning_combination != "No Win":
+            st.success(f"Congratulations! You got a {winning_combination}!")
+        else:
+            st.warning("Sorry, you did not win.")
+
+        st.subheader("Numbers drawn for the queried date range:")
+
+        data = {
+            "Date": [draw["date"] for draw in lottery_data],
+            "Numbers Drawn": [", ".join(str(num) for num in sorted(draw["winning_numbers"])) for draw in lottery_data],
+            "Bonus Number": [draw["bonus_number"] for draw in lottery_data],
+        }
+        df = pd.DataFrame(data)
+        st.write(df)
+if __name__ == "__main__":
+    app()
