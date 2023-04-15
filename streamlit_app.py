5f5a48e4585ad323ed85800be8743c7116b19b2c
bmszone42 <122175396+bmszone42@users.noreply.github.com>
2023-04-14 20:18:00 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,12 +8,12 @@ default_numbers = [9, 36, 41, 44, 59]
 default_bonus_number = 4
 
 # Create inputs for user to enter numbers
-number1 = st.number_input("Enter first number", min_value=1, max_value=69, value=default_numbers[0])
-number2 = st.number_input("Enter second number", min_value=1, max_value=69, value=default_numbers[1])
-number3 = st.number_input("Enter third number", min_value=1, max_value=69, value=default_numbers[2])
-number4 = st.number_input("Enter fourth number", min_value=1, max_value=69, value=default_numbers[3])
-number5 = st.number_input("Enter fifth number", min_value=1, max_value=69, value=default_numbers[4])
-bonus_number = st.number_input("Enter bonus number", min_value=1, max_value=26, value=default_bonus_number)
+number1 = st.sidebar.number_input("Enter first number", min_value=1, max_value=69, value=default_numbers[0])
+number2 = st.sidebar.number_input("Enter second number", min_value=1, max_value=69, value=default_numbers[1])
+number3 = st.sidebar.number_input("Enter third number", min_value=1, max_value=69, value=default_numbers[2])
+number4 = st.sidebar.number_input("Enter fourth number", min_value=1, max_value=69, value=default_numbers[3])
+number5 = st.sidebar.number_input("Enter fifth number", min_value=1, max_value=69, value=default_numbers[4])
+bonus_number = st.sidebar.number_input("Enter bonus number", min_value=1, max_value=26, value=default_bonus_number)
 
 # Get results from website
 url = 'https://www.powerball.com/previous-results?gc=powerball'
