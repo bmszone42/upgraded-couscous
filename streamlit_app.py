dba086e59aa4f008cb8d5ea33bde2c33716ddc99
bmszone42 <122175396+bmszone42@users.noreply.github.com>
2023-04-14 21:01:13 -0400

    Added in megamillions

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,50 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 
-title = "<h1 style='text-align: center; font-family: Arial, sans-serif; color: blue;'>PowerBall Checker -- Let's Check Our Numbers</h1>"
+title = "<h1 style='text-align: center; font-family: Arial, sans-serif; color: blue;'>PowerBall & Mega Millions Checker -- Let's Check Our Numbers</h1>"
 st.markdown(title, unsafe_allow_html=True)
 
+# Function to get Mega Millions numbers and dates
+def get_mega_millions_data():
+    url = 'https://www.megamillions.com/Winning-Numbers/Previous-Drawings.aspx'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    winning_data = soup.select('div.pastDrawDetails')
+
+    winning_numbers = []
+    drawing_dates = []
+    for data in winning_data:
+        date = data.select_one('h5.drawItemDate').text.strip()
+        numbers = data.select('ul.numbers > li.ball')
+        winning_numbers.append([int(n.text) for n in numbers[:-1]] + [int(numbers[-1].text)])  # Last number is Mega Ball
+        drawing_dates.append(date)
+
+    return winning_numbers, drawing_dates
+
+# Function to get Powerball numbers and dates
+def get_powerball_data():
+    url = 'https://www.powerball.com/previous-results?gc=powerball'
+    params = {
+        'game': 'powerball',
+        'p': '',
+        'pn': '',
+        'pp': ''
+    }
+
+    response = requests.get(url, params=params)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    winning_data = soup.select('#searchNumbersResults > div.d-flex.gap-3.flex-column > a')
+
+    winning_numbers = []
+    drawing_dates = []
+    for data in winning_data:
+        date = data.select_one('div > div > div:nth-child(1) > div > h5').text.strip()
+        numbers = data.select('div.white-balls.item-powerball')
+        powerball = data.select('div.powerball.item-powerball')
+        winning_numbers.append([int(n.text) for n in numbers] + [int(p.text) for p in powerball])
+        drawing_dates.append(date)
+
+    return winning_numbers, drawing_dates
 
 def create_html_table(dataframe):
     table_html = '<table border="1" cellpadding="5" cellspacing="0" style="border-collapse: collapse;">'
@@ -39,84 +80,70 @@ number4 = st.sidebar.number_input("Enter fourth number", min_value=1, max_value=
 number5 = st.sidebar.number_input("Enter fifth number", min_value=1, max_value=69, value=default_numbers[4])
 bonus_number = st.sidebar.number_input("Enter bonus number", min_value=1, max_value=26, value=default_bonus_number)
 
-# Get results from website
-url = 'https://www.powerball.com/previous-results?gc=powerball'
-params = {
-    'game': 'powerball',
-    'p': '',
-    'pn': '',
-    'pp': ''
-}
+# Selector for Powerball or Mega Millions
+lottery_game = st.sidebar.selectbox("Select the lottery game", ["Powerball", "Mega Millions"])
 
-response = requests.get(url, params=params)
-
-# Parse the HTML code using BeautifulSoup
-soup = BeautifulSoup(response.content, 'html.parser')
-
-# Find all the divs that contain the winning numbers, their parent divs and the drawing dates
-winning_data = soup.select('#searchNumbersResults > div.d-flex.gap-3.flex-column > a')
-
-# Extract the winning numbers and dates from each winning div
-winning_numbers = []
-drawing_dates = []
-for data in winning_data:
-    date = data.select_one('div > div > div:nth-child(1) > div > h5').text.strip()
-    numbers = data.select('div.white-balls.item-powerball')
-    powerball = data.select('div.powerball.item-powerball')
-    winning_numbers.append([int(n.text) for n in numbers] + [int(p.text) for p in powerball])
-    drawing_dates.append(date)
+# Get the winning numbers and dates based on the user's selection
+if lottery_game == "Powerball":
+    winning_numbers, drawing_dates = get_powerball_data()
+else:
+    winning_numbers, drawing_dates = get_mega_millions_data()
 
 # Create a DataFrame with the winning numbers and dates
 winning_df = pd.DataFrame({'Date': drawing_dates, 'Winning Numbers': winning_numbers})
 
 # Display the winning numbers in an HTML table
-st.write("The winning numbers are:")
+st.write(f"The winning {lottery_game} numbers are:")
 st.markdown(create_html_table(winning_df), unsafe_allow_html=True)
 
 # Define user_numbers
 user_numbers = [number1, number2, number3, number4, number5]
 # Display results
-#st.write(f"You entered the numbers {number1}, {number2}, {number3}, {number4}, {number5}, and {bonus_number}")
 entered_numbers = f"You entered the numbers <strong>{number1}</strong>, <strong>{number2}</strong>, <strong>{number3}</strong>, <strong>{number4}</strong>, <strong>{number5}</strong>, and <span style='color: red;'><strong>{bonus_number}</strong></span>"
 st.markdown(entered_numbers, unsafe_allow_html=True)
 
-def calculate_prize(matched_numbers, matched_bonus):
-    if matched_numbers == 5 and matched_bonus:
-        return "Jackpot"
-    elif matched_numbers == 5:
-        return "$1,000,000"
-    elif matched_numbers == 4 and matched_bonus:
-        return "$50,000"
-    elif matched_numbers == 4:
-        return "$100"
-    elif matched_numbers == 3 and matched_bonus:
-        return "$100"
-    elif matched_numbers == 3:
-        return "$7"
-    elif matched_numbers == 2 and matched_bonus:
-        return "$7"
-    elif matched_numbers == 1 and matched_bonus:
-        return "$4"
-    elif matched_numbers == 0 and matched_bonus:
-        return "$4"
-    else:
-        return "No prize"
+def calculate_prize(lottery_game, matched_numbers, matched_bonus):
+    if lottery_game == "Powerball":
+        prize_structure = {
+            (5, True): "Jackpot",
+            (5, False): "$1,000,000",
+            (4, True): "$50,000",
+            (4, False): "$100",
+            (3, True): "$100",
+            (3, False): "$7",
+            (2, True): "$7",
+            (1, True): "$4",
+            (0, True): "$4"
+        }
+    else:  # Mega Millions
+        prize_structure = {
+            (5, True): "Jackpot",
+            (5, False): "$1,000,000",
+            (4, True): "$10,000",
+            (4, False): "$500",
+            (3, True): "$200",
+            (3, False): "$10",
+            (2, True): "$10",
+            (1, True): "$4",
+            (0, True): "$2"
+        }
+    return prize_structure.get((matched_numbers, matched_bonus), "No prize")
 
 winning_dates = []
 for i, numbers in enumerate(winning_numbers):
     # Check if the user matched the winning numbers
-    matches = set(user_numbers).intersection(numbers[:-1])  # Exclude the last number (powerball)
-    matched_bonus = bonus_number == numbers[-1]  # Check if the bonus number matches the powerball
+    matches = set(user_numbers).intersection(numbers[:-1])  # Exclude the last number (bonus)
+    matched_bonus = bonus_number == numbers[-1]  # Check if the bonus number matches
 
     # If the user has matched any numbers or the bonus number, add the drawing date to the winning_dates list
     if len(matches) > 0 or matched_bonus:
-        prize = calculate_prize(len(matches), matched_bonus)
+        prize = calculate_prize(lottery_game, len(matches), matched_bonus)
         winning_dates.append((drawing_dates[i], prize))
 
 if len(winning_dates) == 0:
     st.write("No winners.")
 else:
-    st.write("You got at least one number on the following drawing dates:")
+    st.write(f"You got at least one number on the following {lottery_game} drawing dates:")
     for date, prize in winning_dates:
         st.write(f"{date}: {prize}")
 
