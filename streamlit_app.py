d33b2eb22bd4ce4d1c63549cb6421b6bdbe3958b
bmszone42 <122175396+bmszone42@users.noreply.github.com>
2023-04-16 12:37:00 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ def get_megamillions_data():
     for data in winning_data:
         #date_string = data.select_one('th > time').attrs['datetime']
         #date = datetime.strptime(date_string, '%Y-%m-%d').strftime('%a, %b %d, %Y')
-        date = data.select_one('th > time').attrs['datetime']).text.strip()
+        date = data.select_one('th > time').attrs['datetime'].text.strip()
         
         numbers = [int(ball.text.strip()) for ball in data.select('li.c-ball--default')]
         megaball = int(data.select_one('li.c-result__bonus-ball > span.c-ball--yellow').text.strip())
