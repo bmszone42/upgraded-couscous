4bd0012c1f2532cf25fbe739e5a2e236a075eb47
bmszone42 <122175396+bmszone42@users.noreply.github.com>
2023-04-16 14:57:23 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,16 +18,11 @@ def get_megamillions_data():
 
     for row in rows:
         date = row.select_one('.c-result-card__title').text.strip()
-        #numbers = [int(n.text) for n in row.select(' .c-ball__label')]
-        #numbers = [int(n.text) for n in row.select('.c-ball .c-result__item .c-ball--default .c-ball__label')]
-        #numbers = [int(n.text) for n in row.select('.c-ball.c-result__item.c-ball--default .c-ball__label')]
-        #numbers = [int(n.text) for n in row.select('c-result-card__result-row.c-result__item.c-ball--default .c-ball__label')]
-        #numbers = [int(n.text) for n in row.select('.c-result__item.c-ball--default span')]
+        megaball = [int(n.text) for n in row.select('.c-result__item.c-result__bonus-ball .c-ball.c-ball--yellow')]
+        
         numbers = [int(n.text) for n in row.select('.c-result__item.c-ball--default > .c-ball__label')]
-        #numbers = [int(n.text) for n in row.select('.c-result__item.c-ball--default .c-ball__label')]
         st.write('Here are the numbers!')
         st.write(numbers)
-        megaball = [int(n.text) for n in row.select('.c-result__item.c-result__bonus-ball .c-ball.c-ball--yellow')]
         #st.write(megaball)
         drawing_dates.append(date)
         winning_numbers.append(numbers + megaball)
