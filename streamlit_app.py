660cf52d1394f0aff5b588e6cb160b57d6079577
bmszone42 <122175396+bmszone42@users.noreply.github.com>
2023-04-16 13:57:18 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ def get_megamillions_data():
 
     for row in rows:
         date = row.select_one('.c-result-card__title').text.strip()
-        numbers = [int(n.text) for n in row.select('.c-result__item > .c-ball__label')]
+        numbers = [int(n.text) for n in row.select('.c-ball > .c-ball__label')]
         #numbers = [int(n.text) for n in row.select('.c-result__item .c-ball__label')]
         st.write("number is: ", numbers)
         megaball = [int(n.text) for n in row.select('.c-result__item .c-ball__yellow')]
