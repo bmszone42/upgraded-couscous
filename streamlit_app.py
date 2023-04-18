a8f8d119177a7f37b1f7b7bc72c3b4c8caa6507c
bmszone42 <122175396+bmszone42@users.noreply.github.com>
2023-04-18 13:37:16 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -119,9 +119,8 @@ for i, default_num in enumerate(default_numbers, start=1):
     col = cols[(i - 1) % 2]
     number_inputs.append(col.slider(f"Enter number {i}", min_value=1, max_value=69, value=default_num, step=1))
 
-# Create a separate column for the bonus number slider
-bonus_col = st.sidebar.column(0)
-bonus_number = bonus_col.slider("Enter bonus number", min_value=1, max_value=26, value=default_bonus_number, step=1)
+# Place bonus number slider in the first column
+bonus_number = cols[0].slider("Enter bonus number", min_value=1, max_value=26, value=default_bonus_number, step=1)
 
 def display_lottery_numbers(numbers, bonus_ball_color):
     html_code = f"""
